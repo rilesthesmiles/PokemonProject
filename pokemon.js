d966da3c6b89ab199b01aee43a9594199c2423b1
Riley_Marshall <95642929+rilesthesmiles@users.noreply.github.com>
2022-10-28 12:22:53 -0600

    Add files via upload

---
+++ pokemon.js
@@ -0,0 +1,105 @@
+/*
+    Description of each position
+    1st = Pokemon Name
+    2nd = Type of Pokemon
+    3rd = Image path for Pokemon
+    4th = Attack Move Name
+    5th = Attack Move Type
+    6th = Attack Move Power
+    7th = Defend Move Name
+    8th = Defend Move Type
+    9th = Defend Move Power
+*/
+
+let pokemonArray = [
+    [
+        "Bulbasaur",
+        "Grass",
+        "images/1",
+        "Tackle",
+        "Normal",
+        5,
+        "Razor Leaf",
+        "Grass",
+        15
+    ],
+    [
+        "Charmander",
+        "Fire",
+        "images/4",
+        "Scratch",
+        "Normal",
+        7,
+        "Ember",
+        "Fire",
+        15
+    ],
+    [
+        "Squirtle",
+        "Water",
+        "images/7",
+        "Scratch",
+        "Normal",
+        7,
+        "Water Gun",
+        "Water",
+        15
+    ],
+    [
+        "Pidgey",
+         "Flying",
+         "images/16",
+         "Tackle",
+         "Normal",
+         5,
+         "Gust",
+         "Flying",
+         12
+    ],
+    [
+        "Rattata",
+        "Normal",
+        "images/19",
+        "Tackle",
+        "Normal",
+        5,
+        "Bite",
+        "Normal",
+        15
+    ],
+    [
+        "Spearow",
+        "Flying",
+        "images/21",
+        "Tackle",
+        "Normal",
+        5,
+        "Peck",
+        "Flying",
+        12
+    ],
+    [
+        "Ekans",
+        "Poison",
+        "images/23",
+        "Wrap",
+        "Normal",
+        5,
+        "Poison Sting",
+        "Poison",
+        12
+    ],
+    [
+        "Pikachu",
+        "Electric",
+        "images/25",
+        "Quick Attack",
+        "Normal",
+        8,
+        "Thundershock",
+        "Electric",
+        15
+    ]
+]
+
+
